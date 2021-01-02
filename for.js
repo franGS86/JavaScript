1e9f0d5ce9e381f74419c78dac01054c4dfceeab
index.html <frankgarita@hotmail.com>
2021-01-02 09:15:20 -0600

    Se agrego el for in, of y label

--- for.js
+++ for.js
@@ -8,12 +8,12 @@ for(let i=0; i<10; i++){
 document.write("<br>");
 
 // for con continue
-document.write("FOR CON CONTINUE "+"<br>"+"<br>");
+document.write("FOR CON CONTINUE se salta el elemento de queramos "+"<br>"+"<br>");
 
 for(let i=0; i<10; i++){
 
     if(i==4){
-        document.write("Se elimino el 4 con la funcion continue"+"<br>");
+        document.write("Se salto el 4 con la funcion continue"+"<br>");
 
         continue;
     }
@@ -22,13 +22,57 @@ for(let i=0; i<10; i++){
 
 }
 
-// for in devuelve el indice
-document.write("<br>"+"FOR IN "+"<br>"+"<br>");
+// for in devuelve el indice delos elementos
+document.write("<br>"+"FOR IN devuelve el indice de los elementos "+"<br>"+"<br>");
+// FOR IN
 
 let animales = ["perro", "gato", "gallina", "perico"];
 
 for (animal in animales) {
    
-    document.write(animales[animal]+"<br>");
+    document.write(animales[animal]+ "=");
+    document.write(animal+"<br>");
+
 
 }
+
+// FOR OF devuelve el valor de los elementos
+document.write("<br>"+"FOR OF devuelve el valor de los elementos "+ "<br>"+"<br>");
+
+
+for (animal of animales) {
+
+    document.write(animal + "<br>");
+
+}
+
+// label
+document.write("<br>"+"FOR OF y FOR OF en conjunto" + "<br>"+"<br>");
+
+let array1 = ["ana", "juan", "pedro", "fran"];
+let array2 = ["rosa", "dylan", "yuki", array1];
+
+for (let array in array2){
+// cuando este en el indice 3 que esta el otro array se recorre
+if(array == 3 ){
+
+    for (let array of array1){
+
+    document.write(array+"<br>");
+
+    }
+
+    }else{
+
+    document.write(array2[array]+"<br>");
+    }
+        
+}
+
+
+
+
+    
+
+
+
